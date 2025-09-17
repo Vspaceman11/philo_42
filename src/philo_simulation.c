72112a6cef16945beb999115337b0e9e6185b3a9
Vspaceman11 <pushkarov.vladyslav@gmai.com>
2025-09-17 12:29:29 +0200

    Fit to norm all functions

--- src/philo_simulation.c
+++ src/philo_simulation.c
@@ -6,7 +6,7 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 13:51:41 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/09/17 11:08:40 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/09/17 12:25:58 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,10 @@ static int	take_forks(t_philo *philo)
 	params = philo->params;
 	left = philo->philo_id;
 	right = (philo->philo_id + 1) % params->n;
-
 	if (philo->philo_id % 2 == 0)
 	{
 		pthread_mutex_lock(&params->forks[right]);
 		print_state(philo, "has taken a fork");
-		if (params->n == 1)
-		{
-			pthread_mutex_unlock(&params->forks[right]);
-			return (0);
-		}
 		pthread_mutex_lock(&params->forks[left]);
 		print_state(philo, "has taken a fork");
 	}
@@ -39,11 +33,6 @@ static int	take_forks(t_philo *philo)
 	{
 		pthread_mutex_lock(&params->forks[left]);
 		print_state(philo, "has taken a fork");
-		if (params->n == 1)
-		{
-			pthread_mutex_unlock(&params->forks[left]);
-			return (0);
-		}
 		pthread_mutex_lock(&params->forks[right]);
 		print_state(philo, "has taken a fork");
 	}
@@ -57,15 +46,12 @@ int	philo_eat(t_philo *philo)
 	params = philo->params;
 	if (!take_forks(philo))
 		return (0);
-
 	pthread_mutex_lock(&philo->meal_mutex);
 	philo->last_meal_time = get_timestamp_ms(params);
 	philo->eat_count++;
 	pthread_mutex_unlock(&philo->meal_mutex);
-
 	print_state(philo, "is eating");
 	smart_sleep(params->time_to_eat, philo);
-
 	pthread_mutex_unlock(&params->forks[philo->philo_id]);
 	pthread_mutex_unlock(&params->forks[(philo->philo_id + 1) % params->n]);
 	return (1);

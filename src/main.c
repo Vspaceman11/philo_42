1777fed4d3cb790df94ab989dcd698a9503bdd4a
Vspaceman11 <pushkarov.vladyslav@gmai.com>
2025-09-19 13:19:43 +0200

    Add comments for main.c

--- src/main.c
+++ src/main.c
@@ -6,12 +6,27 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 13:58:07 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/09/17 12:35:40 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/09/19 13:18:14 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
+/**
+ * @brief Handle the special case when there is only one philosopher.
+ *
+ * In the case of a single philosopher, he can only take one fork,
+ * which makes it impossible to eat. This function simulates the
+ * philosopher taking a fork, waiting until `time_to_die`, and then
+ * printing that the philosopher has died. Afterward, it performs
+ * resource cleanup.
+ *
+ * @param params Pointer to simulation parameters.
+ * @param philos Array of philosopher structures.
+ *
+ * @return 1 if there was only one philosopher and the case was handled,
+ *         0 otherwise.
+ */
 static int	handle_one_philo(t_params *params, t_philo *philos)
 {
 	if (params->n == 1)
@@ -25,6 +40,19 @@ static int	handle_one_philo(t_params *params, t_philo *philos)
 	return (0);
 }
 
+/**
+ * @brief Run the philosopher simulation.
+ *
+ * This function launches the monitor thread to supervise philosophers,
+ * waits until the monitor finishes, then joins all philosopher threads.
+ * After all threads have finished, it cleans up allocated resources.
+ *
+ * @param params Pointer to simulation parameters.
+ * @param philos Array of philosopher structures.
+ *
+ * @return 0 on success, or a non-zero error code if the monitor thread
+ *         could not be created.
+ */
 static int	run_simulation(t_params *params, t_philo *philos)
 {
 	pthread_t	monitor;
